4ac2dfd63edd33bfb780249e0cbd435a7ed6854a
Xiaobao Zeng <xiaobao.zeng@shopee.com>
2019-10-23 20:55:43 +0800

    use func option

--- breaker/circuit_break.go
+++ breaker/circuit_break.go
@@ -8,20 +8,33 @@ import (
 )
 
 var (
-	defaultConfig = Config{
-		RefreshInterval:         5 * 60 * 1000,
-		ErrorThresholdPercent:   20,
-		RequestVolumeThreshold:  1000,
+	defaultOpenConfig = CircuitBreakerOpenConfig{
+		RefreshInterval:        5 * 60 * 1000,
+		ErrorThresholdPercent:  20,
+		RequestVolumeThreshold: 1000,
+	}
+
+	defaultCloseConfig = CircuitBreakerCloseConfig{
 		SuccessThresholdPercent: 85,
 		RecoveryInterval:        30 * 1000,
 		SuccessVolumeThreshold:  100,
-		Callback:                nil,
 	}
 )
 
 var (
 	ErrTooManyErrors           = errors.New("too many errors")
 	ErrorManualDroppingRequest = errors.New("manual dropping requests")
+)
+
+const (
+	minRefreshInterval = time.Minute
+	maxRefreshInterval = time.Minute * 5
+
+	minSleepWindow = time.Second * 15
+	maxSleepWindow = minRefreshInterval
+
+	minRecoveryInterval = time.Second * 30
+	maxRecoveryInterval = maxRefreshInterval
 )
 
 // type CircuitBreakerStatus int32
@@ -43,81 +56,102 @@ const (
 	minErrorThresholdPercent uint8 = 5
 )
 
-// type OperationMode int32
+//CircuitBreakerOpenConfig case in which circuit breaker turns to open
+type CircuitBreakerOpenConfig struct {
+	RefreshInterval        time.Duration //statistical period. unit: ms
+	ErrorThresholdPercent  uint8         //circuitBreaker turns to open when errors up to it in refresh interval. take effect with RequestVolumeThreshold
+	RequestVolumeThreshold uint32        //circuitBreaker turns to open when errors up to ErrorThresholdPercent and volume comes to it. take effect with ErrorThresholdPercent
 
-// //circuitBreaker take effect only in auto mode
-// const (
-// 	OperationModeAuto OperationMode = iota + 1
-// 	OperationModeManual
-// )
-
-const (
-	OperationModeAuto int32 = iota + 1
-	OperationModeManual
-)
-
-type Config struct {
-	RefreshInterval        uint64 //statistical period. unit: ms
-	ErrorThresholdPercent  uint8  //circuitBreaker turns to open when errors up to it in refresh interval. take effect with RequestVolumeThreshold
-	RequestVolumeThreshold uint32 //circuitBreaker turns to open when errors up to ErrorThresholdPercent and volume comes to it. take effect with ErrorThresholdPercent
-
-	SleepWindow uint32 //after SleepWindow(ms), circuitBreaker turns to half-open when circuitBreaker is open
-
-	SuccessThresholdPercent uint8 //circuitBreaker turns to closed when successes up to it in refresh interval. take effect with SuccessVolumeThreshold
-	RecoveryInterval        uint64
-	SuccessVolumeThreshold  uint32 //circuitBreaker turns to closed when successes up to SuccessThresholdPercent and volume comes to it. take effect with SuccessThresholdPercent
-
-	Callback func() error //callback when circuitBreak turns to open from closed or to closed from half-open
+	errorVolumeThreshold uint32 //RequestVolumeThreshold * (ErrorThresholdPercent / 100)
 }
 
+//CircuitBreakerCloseConfig case in which circuit breaker turns to closed
+type CircuitBreakerCloseConfig struct {
+	RecoveryInterval        time.Duration
+	SuccessThresholdPercent uint8  //circuitBreaker turns to closed when successes up to it in refresh interval. take effect with SuccessVolumeThreshold
+	SuccessVolumeThreshold  uint32 //circuitBreaker turns to closed when successes up to SuccessThresholdPercent and volume comes to it. take effect with SuccessThresholdPercent
+
+	successVolumeThreshold uint32 //SuccessVolumeThreshold * (SuccessThresholdPercent / 100)
+}
+
+type CircuitBreakerOption func(c *CircuitBreaker)
+
+func SetOpenConfig(oc CircuitBreakerOpenConfig) CircuitBreakerOption {
+	return func(c *CircuitBreaker) {
+		if oc.RefreshInterval >= minRefreshInterval && oc.RefreshInterval <= maxRefreshInterval {
+			c.openConfig = oc
+		}
+	}
+}
+
+func SetCloseConfig(cc CircuitBreakerCloseConfig) CircuitBreakerOption {
+	return func(c *CircuitBreaker) {
+		if cc.RecoveryInterval >= minRecoveryInterval && cc.RecoveryInterval <= maxRecoveryInterval {
+			c.closeConfig = cc
+		}
+	}
+}
+
+func SetSleepWindow(t time.Duration) CircuitBreakerOption {
+	return func(c *CircuitBreaker) {
+		if t >= minSleepWindow && t <= maxSleepWindow {
+			c.sleepWindow = t
+		}
+	}
+}
+
+func SetCallback(f func()) CircuitBreakerOption {
+	return func(c *CircuitBreaker) {
+		if f != nil {
+			c.callback = f
+		}
+	}
+}
+
+//CircuitBreaker
 type CircuitBreaker struct {
 	status        int32
-	operationMode int32
+	requestVolume uint32 //total num of request
 
-	config Config
+	openConfig  CircuitBreakerOpenConfig
+	errorVolume uint32
 
-	errorVolumeThreshold   uint32
-	successVolumeThreshold uint32
+	sleepWindow time.Duration //after SleepWindow, circuitBreaker turns to half-open when circuitBreaker is open
+
+	closeConfig   CircuitBreakerCloseConfig
+	successVolume uint32
+
+	callback func() //callback when circuitBreak turns to open from closed or to closed from half-open
 
 	start     time.Time //start of statistical period
 	eventTime time.Time //last time at which circuit breaker is open
 
-	requestVolume uint32
-	errorVolume   uint32
-	mu            sync.Mutex
+	mu sync.Mutex
 }
 
-//
-func New(config ...Config) *CircuitBreaker {
-	breaker := new(CircuitBreaker)
-	breaker.status = CircuitBreakerStatusClosed
-	breaker.operationMode = OperationModeAuto
+//New return a new citcuit breaker
+func New(opts ...CircuitBreakerOption) *CircuitBreaker {
+	c := &CircuitBreaker{
+		status:        CircuitBreakerStatusClosed,
+		requestVolume: 0,
 
-	breaker.config = defaultConfig
-	if len(config) != 0 {
-		breaker.config = config[0]
+		openConfig:  defaultOpenConfig,
+		errorVolume: 0,
+
+		sleepWindow: time.Minute * 3,
+
+		closeConfig: defaultCloseConfig,
+
+		callback: nil,
+
+		start: time.Now(),
 	}
 
-	if breaker.config.ErrorThresholdPercent > maxErrorThresholdPercent {
-		breaker.config.ErrorThresholdPercent = maxErrorThresholdPercent
-	}
-	if breaker.config.ErrorThresholdPercent < minErrorThresholdPercent {
-		breaker.config.ErrorThresholdPercent = minErrorThresholdPercent
+	for _, opt := range opts {
+		opt(c)
 	}
 
-	breaker.errorVolumeThreshold = uint32(float32(breaker.config.RequestVolumeThreshold) * (float32)(breaker.config.ErrorThresholdPercent) / (float32)(100))
-
-	breaker.start = time.Now()
-
-	return breaker
-}
-
-func (c *CircuitBreaker) SetOperationMode(om int32) {
-	atomic.StoreInt32(&c.operationMode, int32(om))
-}
-
-func (c *CircuitBreaker) SetStatus(st int32) {
-	atomic.StoreInt32(&c.status, int32(st))
+	return c
 }
 
 //ReportRequest is a short hand of ReportRequestN
